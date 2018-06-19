adde3478f363291b85cd18ffa80498c23c7d4b7d
stevenGuo1995 <stevenguo1995@gmail.com>
2018-06-19 14:33:46 +0800

    leetcode

--- 58.lengthofLastword.py
+++ 58.lengthofLastword.py
@@ -1,3 +1,16 @@
+"""
+题目：
+
+58. 最后一个单词的长度
+
+给定一个仅包含大小写字母和空格 ' ' 的字符串，返回其最后一个单词的长度。
+
+如果不存在最后一个单词，请返回 0 。
+
+说明：一个单词是指由字母组成，但不包含任何空格的字符串。
+"""
+
+
 class Solution:
     def lengthOfLastWord(self, s):
         """
@@ -12,5 +25,5 @@ class Solution:
 
 if __name__ == "__main__":
     s = Solution()
-    r = s.lengthOfLastWord('   ')
+    r = s.lengthOfLastWord('hello world nihao')
     print(r)
---
+++ 67.addbinary.py
@@ -0,0 +1,16 @@
+"""
+67.二进制求和
+
+给定两个二进制字符串，返回他们的和（用二进制表示）。
+
+输入为非空字符串且只包含数字 1 和 0。
+"""
+
+
+class Solution:
+    def addBinary(self, a, b):
+        """
+        :type a: str
+        :type b: str
+        :rtype: str
+        """
